3018fecbf6246a3b7ee025ca5f4294a7a0f364d0
Lays Rodrigues <laysrodriguessilva@gmail.com>
2021-06-25 20:38:45 -0300

    tests: Add test gilded rose object creation
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -11,6 +11,12 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEquals("foo", items[0].name)
 
+    def test_gilded_rose_creation(self):
+        items = [Item('Aged Brie', 0,0)]
+        gilded_rose = GildedRose(items)
+        assert isinstance(gilded_rose, GildedRose)
+        assert len(gilded_rose.items) is 1
+
         
 if __name__ == '__main__':
     unittest.main()
