e7c8fb743e2282ba87509c9153fed23171e825fc
Lays Rodrigues <laysrodriguessilva@gmail.com>
2021-06-25 20:17:12 -0300

    tests: Add tests of Item class
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

---
+++ python/test_item_class.py
@@ -0,0 +1,19 @@
+import unittest
+
+from gilded_rose import Item
+
+class ItemTest(unittest.TestCase):
+
+    def test_create_item(self):
+        item = Item(name='Aged Brie', sell_in=10, quality=1)
+        assert isinstance(item, Item)
+        assert item.name == 'Aged Brie'
+        assert item.sell_in == 10
+        assert item.quality ==1
+
+    def test_repr_item(self):
+        item = repr(Item(name='Aged Brie', sell_in=10, quality=1))
+        assert item == 'Aged Brie, 10, 1'
+
+if __name__ == '__main__':
+    unittest.main()
