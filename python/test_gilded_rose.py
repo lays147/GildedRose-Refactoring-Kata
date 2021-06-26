072ee26f4193af79b383da90512430d9c4daa84f
Lays Rodrigues <laysrodriguessilva@gmail.com>
2021-06-26 00:16:25 -0300

    tests: Add update quality test for defined case one
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -17,6 +17,43 @@ class GildedRoseTest(unittest.TestCase):
         assert isinstance(gilded_rose, GildedRose)
         assert len(gilded_rose.items) is 1
 
+    def test_update_quality_case_one(self):
+        """
+        [One] 
+            Conditions:
+                item name is not Aged Brie and is not Backstage
+                item has quality > 0
+                item name is not Sulfuras
+            Result: 
+                item quality is decreased by one
+        [Two]
+            Condition:
+                item name is not Sulfuras
+            Result:
+                item sell_in is decreased by one
+        [Three]
+            Conditions:
+                item sell_in is > 0
+            Result:
+                item quality remains the same
         
+        Test Case:
+            item = Item('Item', sell_in = 2, quality = 5)
+            [One] -> item.quality = 4
+            [Two] -> item.sell_in = 1
+            [Three] -> item.quality = 4
+        
+        Expected Result:
+            item.quality == 4
+            item.sell_in == 1 
+            item.name == Item
+        """
+        items = [Item(name='Item', sell_in=2, quality = 5)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        assert gilded_rose.items[0].quality is 4
+        assert gilded_rose.items[0].sell_in is 1
+        assert gilded_rose.items[0].name is 'Item'
+
 if __name__ == '__main__':
     unittest.main()
