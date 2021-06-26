727be7fded4d1b6a968ce4d3f60c72b362af8ff9
Lays Rodrigues <laysrodriguessilva@gmail.com>
2021-06-26 16:17:31 -0300

    feat: Format files with black
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- python/tests/test_item_class.py
+++ python/tests/test_item_class.py
@@ -2,15 +2,15 @@ import unittest
 
 from kata.gilded_rose import Item
 
-class ItemTest(unittest.TestCase):
 
+class ItemTest(unittest.TestCase):
     def test_create_item(self):
-        item = Item(name='Aged Brie', sell_in=10, quality=1)
+        item = Item(name="Aged Brie", sell_in=10, quality=1)
         assert isinstance(item, Item)
-        assert item.name == 'Aged Brie'
+        assert item.name == "Aged Brie"
         assert item.sell_in == 10
-        assert item.quality ==1
+        assert item.quality == 1
 
     def test_repr_item(self):
-        item = repr(Item(name='Aged Brie', sell_in=10, quality=1))
-        assert item == 'Aged Brie, 10, 1'
+        item = repr(Item(name="Aged Brie", sell_in=10, quality=1))
+        assert item == "Aged Brie, 10, 1"
