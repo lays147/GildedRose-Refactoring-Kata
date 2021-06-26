8f38960d46313f2b1afaf25ae050b03d5119178c
Lays Rodrigues <laysrodriguessilva@gmail.com>
2021-06-26 15:17:08 -0300

    tests: Add update quality test for defined case nine
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -316,5 +316,47 @@ class GildedRoseTest(unittest.TestCase):
         assert gilded_rose.items[0].sell_in is 4
         assert gilded_rose.items[0].name is 'Backstage passes to a TAFKAL80ETC concert'
 
+    def test_update_quality_case_nine(self):
+        """
+        [One] 
+            Conditions:
+                item name is not Aged Brie and is not Backstage
+                item has quality > 0
+                item name is not Sulfuras
+            Result: 
+                item quality is decreased by one
+        [Two]
+            Condition:
+                item name is not Sulfuras
+            Result:
+                item sell_in is decreased by one
+        [Three]
+            Conditions:
+                item sell_in is < 0
+                item name is not Aged Brie
+                item name is not Backstage
+                item quality > 0
+                item name is not Sulfuras
+            Result:
+                item quality is decreased by one
+        
+        Test Case:
+            item = Item('Item', sell_in = 0, quality = 5)
+            [One] -> item.quality = 4
+            [Two] -> item.sell_in = -1
+            [Three] -> item.quality = 3
+        
+        Expected Result:
+            item.quality == 3
+            item.sell_in == -1 
+            item.name == Item
+        """
+        items = [Item(name='Item', sell_in=0, quality = 5)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        assert gilded_rose.items[0].quality is 3
+        assert gilded_rose.items[0].sell_in is -1
+        assert gilded_rose.items[0].name is 'Item'
+
 if __name__ == '__main__':
     unittest.main()
