2c9f482c714b704d98be5924d51ebb89a26cf20f
Lays Rodrigues <laysrodriguessilva@gmail.com>
2021-06-27 12:19:18 -0300

    refactor: Moving to py3 - Cleanup unicode and fix imports
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from kata.gilded_rose import Item, GildedRose
 
 
--- python/tests/texttest_fixture.py
+++ python/tests/texttest_fixture.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 from __future__ import print_function
 
-from gilded_rose import *
+from kata.gilded_rose import *
 
 if __name__ == "__main__":
     print("OMGHAI!")
